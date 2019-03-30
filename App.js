95447cd99956fd002fac7d8c207f4820b8d59c3d
yaojie <limyaojie93@gmail.com>
2019-03-30 21:30:33 +0800

    add more buttons to second screen

--- App.js
+++ App.js
@@ -18,16 +18,41 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     const localizedStrings = {
-      cn: { hello: "哈咯" },
-      en: { hello: "hello" },
+      cn: {
+        title: "健康预约",
+        problem: "哪里痛?",
+        eye: "眼睛",
+        throat: "喉咙",
+        chest: "胸口",
+        stomach: "肚子",
+        others: "其他"
+      },
+      en: {
+        title: "Healthcare Appointment",
+        problem: "Where Pain?",
+        eye: "Eye",
+        throat: "Throat",
+        chest: "Chest",
+        stomach: "Stomach",
+        others: "Others"
+      },
     }
     this.localeStore = new Proxy({
       locale: 'en',
     }, {
       get(target, property) {
-        return localizedStrings[target.locale][property]
+        if (localizedStrings[target.locale][property]) {
+          return localizedStrings[target.locale][property]
+        } else {
+            if (target.locale !== 'en' && localizedStrings['en'][property]) {
+              return localizedStrings['en'][property]
+            } else {
+              return property
+            }
+          }
+        }
       }
-    })
+    )
   }
   changeLocale(key) {
     this.localeStore.locale = key;
--- screens/ProblemScreen.js
+++ screens/ProblemScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { HeaderText } from '../HeaderText';
 
 export default class ProblemScreen extends React.Component {
   constructor(props) {
@@ -7,8 +9,24 @@ export default class ProblemScreen extends React.Component {
   }
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{this.props.screenProps.localeStore.hello }</Text>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{marginBottom: 'auto', paddingTop: 60}}>
+          <Text>{this.props.screenProps.localeStore.title}</Text>
+        </View>
+        <View style={{marginBottom: 'auto', alignItems: 'center', width: '100%'}}>
+          <HeaderText>{this.props.screenProps.localeStore.problem }</HeaderText>
+          {
+            ["eye", "throat", "chest", "stomach", "others"].map(part =>
+              <View key={part} style={{width: '50%', paddingBottom: 15, paddingTop: 15}}>
+                <Button
+                  title={this.props.screenProps.localeStore[part]}
+                  color="black"
+                  titleStyle={{fontSize: 32}}
+                />
+              </View>
+            )
+          }
+        </View>
       </View>
     );
   }
